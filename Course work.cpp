29c360ae9f1f6547e08fb6e466bfc23614177e0c
VADUMr <115543904+VADUMr@users.noreply.github.com>
2024-11-10 22:04:26 +0200

    Надіємо що на цьому більше сюди не заліземо

--- Course work.cpp
+++ Course work.cpp
@@ -5,19 +5,22 @@
 #include <memory>
 
 // Crypto++ включення
-#include <cryptopp/aes.h>
-#include <cryptopp/des.h>
-#include <cryptopp/blowfish.h>
-#include <cryptopp/modes.h>
-#include <cryptopp/filters.h>
-#include <cryptopp/rsa.h>
-#include <cryptopp/osrng.h>
-#include <cryptopp/sha.h>
-#include <cryptopp/sha3.h>
-#include <cryptopp/hex.h>
-#include <cryptopp/gcm.h>
-#include <cryptopp/dsa.h>
-#include <cryptopp/base64.h>
+#include <aes.h>
+#include <des.h>
+#include <blowfish.h>
+#include <modes.h>
+#include <filters.h>
+#include <rsa.h>
+#include <osrng.h>
+#include <sha.h>
+#include <sha3.h>
+#include <hex.h>
+#include <gcm.h>
+#include <dsa.h>
+#include <base64.h>
+#include <dsa.h>
+#include <hex.h>
+#include <osrng.h>
 
 using namespace std;
 using namespace CryptoPP;
@@ -53,6 +56,16 @@ public:
         rng.GenerateBlock(desKey, DES::DEFAULT_KEYLENGTH);
         rng.GenerateBlock(blowfishKey, BLOWFISH_DEFAULT_KEYLENGTH);
 
+        // Генерація IV для кожного алгоритму
+        rng.GenerateBlock(aesIV, AES::BLOCKSIZE);
+        rng.GenerateBlock(desIV, DES::BLOCKSIZE);
+        rng.GenerateBlock(blowfishIV, Blowfish::BLOCKSIZE);
+
+        // Генерація додаткових IV для різних режимів AES
+        rng.GenerateBlock(aesCBCIV, AES::BLOCKSIZE);
+        rng.GenerateBlock(aesCTRIV, AES::BLOCKSIZE);
+        rng.GenerateBlock(aesGCMIV, AES::BLOCKSIZE);
+
         // Генерація RSA ключів
         RSA::PrivateKey privateKey;
         privateKey.GenerateRandomWithKeySize(rng, 3072);
@@ -62,8 +75,10 @@ public:
         // Генерація DSA ключів
         DSA::PrivateKey dsaPrivateKey;
         dsaPrivateKey.GenerateRandomWithKeySize(rng, 2048);
-        dsaPublicKey = DSA::PublicKey(dsaPrivateKey);
         this->dsaPrivateKey = dsaPrivateKey;
+        DSA::PublicKey dsaPublicKey;
+        dsaPublicKey.AssignFrom(dsaPrivateKey);
+        this->dsaPublicKey = dsaPublicKey;
     }
 
     // Методи для симетричного шифрування
@@ -93,7 +108,6 @@ public:
         }
     }
 
-    // RSA шифрування/дешифрування
     string encryptRSA(const string& plaintext) {
         return executeRSAEncryption(plaintext);
     }
@@ -102,7 +116,6 @@ public:
         return executeRSADecryption(ciphertext);
     }
 
-    // Методи хешування
     string calculateHash(const string& data, HashAlgorithm algo) {
         switch (algo) {
         case HashAlgorithm::SHA256:
@@ -114,7 +127,6 @@ public:
         }
     }
 
-    // Методи для цифрового підпису
     string signMessage(const string& message) {
         return createDigitalSignature(message);
     }
@@ -131,22 +143,30 @@ private:
     byte aesKey[AES::DEFAULT_KEYLENGTH];
     byte desKey[DES::DEFAULT_KEYLENGTH];
     byte blowfishKey[BLOWFISH_DEFAULT_KEYLENGTH];
+
+    // IV для кожного алгоритму
+    byte aesIV[AES::BLOCKSIZE];
+    byte desIV[DES::BLOCKSIZE];
+    byte blowfishIV[Blowfish::BLOCKSIZE];
+
+    // Додаткові IV для різних режимів AES
+    byte aesCBCIV[AES::BLOCKSIZE];
+    byte aesCTRIV[AES::BLOCKSIZE];
+    byte aesGCMIV[AES::BLOCKSIZE];
+
     RSA::PublicKey rsaPublicKey;
     RSA::PrivateKey rsaPrivateKey;
     DSA::PublicKey dsaPublicKey;
     DSA::PrivateKey dsaPrivateKey;
 
-    // AES шифрування з різними режимами
     string encryptAES(const string& plaintext, AESMode mode) {
         string ciphertext;
         AutoSeededRandomPool rng;
-        byte iv[AES::BLOCKSIZE];
-        rng.GenerateBlock(iv, AES::BLOCKSIZE);
 
         switch (mode) {
         case AESMode::CBC: {
             CBC_Mode<AES>::Encryption encryptor;
-            encryptor.SetKeyWithIV(aesKey, sizeof(aesKey), iv);
+            encryptor.SetKeyWithIV(aesKey, sizeof(aesKey), aesCBCIV);
             StringSource(plaintext, true,
                 new StreamTransformationFilter(encryptor,
                     new StringSink(ciphertext)
@@ -156,7 +176,7 @@ private:
         }
         case AESMode::CTR: {
             CTR_Mode<AES>::Encryption encryptor;
-            encryptor.SetKeyWithIV(aesKey, sizeof(aesKey), iv);
+            encryptor.SetKeyWithIV(aesKey, sizeof(aesKey), aesCTRIV);
             StringSource(plaintext, true,
                 new StreamTransformationFilter(encryptor,
                     new StringSink(ciphertext)
@@ -166,7 +186,7 @@ private:
         }
         case AESMode::GCM: {
             GCM<AES>::Encryption encryptor;
-            encryptor.SetKeyWithIV(aesKey, sizeof(aesKey), iv, sizeof(iv));
+            encryptor.SetKeyWithIV(aesKey, sizeof(aesKey), aesGCMIV, sizeof(aesGCMIV));
             StringSource(plaintext, true,
                 new AuthenticatedEncryptionFilter(encryptor,
                     new StringSink(ciphertext)
@@ -180,10 +200,11 @@ private:
 
     string decryptAES(const string& ciphertext, AESMode mode) {
         string decryptedtext;
+
         switch (mode) {
         case AESMode::CBC: {
             CBC_Mode<AES>::Decryption decryptor;
-            decryptor.SetKeyWithIV(aesKey, sizeof(aesKey), nullptr);
+            decryptor.SetKeyWithIV(aesKey, sizeof(aesKey), aesCBCIV);
             StringSource(ciphertext, true,
                 new StreamTransformationFilter(decryptor,
                     new StringSink(decryptedtext)
@@ -193,7 +214,7 @@ private:
         }
         case AESMode::CTR: {
             CTR_Mode<AES>::Decryption decryptor;
-            decryptor.SetKeyWithIV(aesKey, sizeof(aesKey), nullptr);
+            decryptor.SetKeyWithIV(aesKey, sizeof(aesKey), aesCTRIV);
             StringSource(ciphertext, true,
                 new StreamTransformationFilter(decryptor,
                     new StringSink(decryptedtext)
@@ -203,7 +224,7 @@ private:
         }
         case AESMode::GCM: {
             GCM<AES>::Decryption decryptor;
-            decryptor.SetKeyWithIV(aesKey, sizeof(aesKey), nullptr, 0);
+            decryptor.SetKeyWithIV(aesKey, sizeof(aesKey), aesGCMIV, sizeof(aesGCMIV));
             StringSource(ciphertext, true,
                 new AuthenticatedDecryptionFilter(decryptor,
                     new StringSink(decryptedtext)
@@ -215,38 +236,38 @@ private:
         return decryptedtext;
     }
 
-    // DES шифрування
     string encryptDES(const string& plaintext) {
         string ciphertext;
         CBC_Mode<DES>::Encryption encryptor;
-        encryptor.SetKey(desKey, sizeof(desKey));
+        encryptor.SetKeyWithIV(desKey, sizeof(desKey), desIV);
 
         StringSource(plaintext, true,
             new StreamTransformationFilter(encryptor,
                 new StringSink(ciphertext)
             )
         );
+
         return ciphertext;
     }
 
     string decryptDES(const string& ciphertext) {
         string decryptedtext;
         CBC_Mode<DES>::Decryption decryptor;
-        decryptor.SetKey(desKey, sizeof(desKey));
+        decryptor.SetKeyWithIV(desKey, sizeof(desKey), desIV);
 
         StringSource(ciphertext, true,
             new StreamTransformationFilter(decryptor,
                 new StringSink(decryptedtext)
             )
         );
+
         return decryptedtext;
     }
 
-    // Blowfish шифрування
     string encryptBlowfish(const string& plaintext) {
         string ciphertext;
         CBC_Mode<Blowfish>::Encryption encryptor;
-        encryptor.SetKey(blowfishKey, sizeof(blowfishKey));
+        encryptor.SetKeyWithIV(blowfishKey, sizeof(blowfishKey), blowfishIV);
 
         StringSource(plaintext, true,
             new StreamTransformationFilter(encryptor,
@@ -259,7 +280,7 @@ private:
     string decryptBlowfish(const string& ciphertext) {
         string decryptedtext;
         CBC_Mode<Blowfish>::Decryption decryptor;
-        decryptor.SetKey(blowfishKey, sizeof(blowfishKey));
+        decryptor.SetKeyWithIV(blowfishKey, sizeof(blowfishKey), blowfishIV);
 
         StringSource(ciphertext, true,
             new StreamTransformationFilter(decryptor,
@@ -269,11 +290,9 @@ private:
         return decryptedtext;
     }
 
-    // RSA шифрування/дешифрування
     string executeRSAEncryption(const string& plaintext) {
         string ciphertext;
         AutoSeededRandomPool rng;
-
         RSAES_OAEP_SHA_Encryptor encryptor(rsaPublicKey);
         StringSource(plaintext, true,
             new PK_EncryptorFilter(rng, encryptor,
@@ -286,7 +305,6 @@ private:
     string executeRSADecryption(const string& ciphertext) {
         string decryptedtext;
         AutoSeededRandomPool rng;
-
         RSAES_OAEP_SHA_Decryptor decryptor(rsaPrivateKey);
         StringSource(ciphertext, true,
             new PK_DecryptorFilter(rng, decryptor,
@@ -296,11 +314,9 @@ private:
         return decryptedtext;
     }
 
-    // Методи хешування
     string calculateSHA256(const string& data) {
         string digest;
         SHA256 hash;
-
         StringSource(data, true,
             new HashFilter(hash,
                 new HexEncoder(
@@ -314,7 +330,6 @@ private:
     string calculateSHA3(const string& data) {
         string digest;
         SHA3_256 hash;
-
         StringSource(data, true,
             new HashFilter(hash,
                 new HexEncoder(
@@ -325,11 +340,9 @@ private:
         return digest;
     }
 
-    // Цифровий підпис
     string createDigitalSignature(const string& message) {
         string signature;
         AutoSeededRandomPool rng;
-
         DSA::Signer signer(dsaPrivateKey);
         StringSource(message, true,
             new SignerFilter(rng, signer,
@@ -342,7 +355,6 @@ private:
     bool verifyDigitalSignature(const string& message, const string& signature) {
         bool result = false;
         DSA::Verifier verifier(dsaPublicKey);
-
         StringSource(signature + message, true,
             new SignatureVerificationFilter(verifier,
                 new ArraySink((byte*)&result, sizeof(result))
@@ -351,9 +363,9 @@ private:
         return result;
     }
 };
-
 // Приклад використання
 int main() {
+    setlocale(LC_ALL, "ukr");
     try {
         Encryptor encryptor;
         string message = "Тестове повідомлення для шифрування";
@@ -363,25 +375,58 @@ int main() {
         // Тестування різних режимів AES
         cout << "=== AES Шифрування ===" << endl;
         string aesEncryptedCBC = encryptor.encryptSymmetric(message, SymmetricAlgorithm::AES, AESMode::CBC);
+        cout << "AES CBC режим - зашифроване: " << aesEncryptedCBC << endl;
         string aesDecryptedCBC = encryptor.decryptSymmetric(aesEncryptedCBC, SymmetricAlgorithm::AES, AESMode::CBC);
         cout << "AES CBC режим - дешифроване: " << aesDecryptedCBC << endl;
 
         string aesEncryptedCTR = encryptor.encryptSymmetric(message, SymmetricAlgorithm::AES, AESMode::CTR);
+        cout << "AES CTR режим - зашифроване: " << aesEncryptedCTR << endl;
         string aesDecryptedCTR = encryptor.decryptSymmetric(aesEncryptedCTR, SymmetricAlgorithm::AES, AESMode::CTR);
         cout << "AES CTR режим - дешифроване: " << aesDecryptedCTR << endl;
 
         // Тестування DES
         cout << "\n=== DES Шифрування ===" << endl;
         string desEncrypted = encryptor.encryptSymmetric(message, SymmetricAlgorithm::DES);
+        cout << "DES - зашифроване: " << desEncrypted << endl;
         string desDecrypted = encryptor.decryptSymmetric(desEncrypted, SymmetricAlgorithm::DES);
         cout << "DES - дешифроване: " << desDecrypted << endl;
 
         // Тестування Blowfish
         cout << "\n=== Blowfish Шифрування ===" << endl;
         string blowfishEncrypted = encryptor.encryptSymmetric(message, SymmetricAlgorithm::BLOWFISH);
+        cout << "Blowfish - зашифроване: " << blowfishEncrypted << endl;
         string blowfishDecrypted = encryptor.decryptSymmetric(blowfishEncrypted, SymmetricAlgorithm::BLOWFISH);
         cout << "Blowfish - дешифроване: " << blowfishDecrypted << endl;
 
         // Тестування хешування
         cout << "\n=== Хешування ===" << endl;
-        string sha256Hash = enc
+        string sha256Hash = encryptor.calculateHash(message, HashAlgorithm::SHA256);
+        cout << "SHA-256 хеш: " << sha256Hash << endl;
+
+        string sha3Hash = encryptor.calculateHash(message, HashAlgorithm::SHA3_256);
+        cout << "SHA3-256 хеш: " << sha3Hash << endl;
+
+        // Тестування RSA
+        cout << "\n=== RSA Шифрування ===" << endl;
+        string rsaEncrypted = encryptor.encryptRSA(message);
+        cout << "RSA - зашифроване: " << rsaEncrypted << endl;
+        string rsaDecrypted = encryptor.decryptRSA(rsaEncrypted);
+        cout << "RSA - дешифроване: " << rsaDecrypted << endl;
+
+        // Тестування цифрового підпису
+        cout << "\n=== Цифровий підпис ===" << endl;
+        string signature = encryptor.signMessage(message);
+        bool isValid = encryptor.verifySignature(message, signature);
+        cout << "Перевірка підпису: " << (isValid ? "Успішно" : "Помилка") << endl;
+
+
+    }
+    catch (const Exception& e) {
+        cerr << "Crypto++ помилка: " << e.what() << endl;
+    }
+    catch (const exception& e) {
+        cerr << "Стандартна помилка: " << e.what() << endl;
+    }
+
+    return 0;
+}
